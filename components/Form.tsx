b389c9e991871200b002e888eb411f3a8d765402
Eric Ming <ericmingyt@gmail.com>
2023-01-08 15:06:57 -0500

    Small typo fix

--- components/Form.tsx
+++ components/Form.tsx
@@ -361,7 +361,7 @@ const Form: FunctionComponent = () => {
                 <div className="flex items-center h-5">
                     <input id="terms" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" required />
                 </div>
-                <label htmlFor="terms" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">I agree with the <a href="https://leagueplaywaiver.tiiny.site/" target="_blank" className="text-blue-600 hover:underline dark:text-blue-500">terms and conditions</a></label>
+                <label htmlFor="terms" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">I agree with the <a href="https://leagueplaywaiver.tiiny.site/" target="_blank" rel="noreferrer" className="text-blue-600 hover:underline dark:text-blue-500">terms and conditions</a></label>
             </div>
             <button role="link" onClick={handleCheckout} type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                 Pay and Sign Up
